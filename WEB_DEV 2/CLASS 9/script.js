a5d30484aa9bbf0552de4fc8bb76108c294c10c4
tarunsharma6969 <tarunsharma26062007@gmail.com>
2026-02-03 15:49:04 +0530

    Add files via upload

---
+++ WEB_DEV 2/CLASS 9/script.js
@@ -0,0 +1,20 @@
+const task1 = document.getElementById
+("profile")
+task1.style.backgroundColor = "tomato";
+task1.style.padding = "15px";
+task1.textAlign="center";
+const task2 = document.getElementsByClassName("important");
+console.log(task2);
+for(let i=0;i<task2.length;i++){
+   task2[i].style.color="purple";    
+   task2[i].style.fontSize="40px";
+}
+console.log(task2.length);
+
+const task3 = document.querySelectorAll(".important");
+("content");
+const task3= ContentVisibilityAutoStateChangeEvent.querySelectorAll
+("p");
+for(let i=0;i<task3.length;i++){
+    task3[i].style.backgroundColor="cyan";
+}
